60147beb126609fce73c8d5c2b24954a7b32161b
s18002 <s18002@std.it-college.ac.jp>
2020-02-07 11:00:02 +0900

    てすと3

--- app/src/main/java/com/example/soba/Yonabaru.kt
+++ app/src/main/java/com/example/soba/Yonabaru.kt
@@ -1,7 +1,10 @@
 package com.example.soba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Yonabaru : AppCompatActivity() {
 
@@ -9,7 +12,16 @@ class Yonabaru : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yonabaru)
 
-
-
+        //1.画面遷移用ボタンの取得。
+        val btnIntent = findViewById<Button>(R.id.button)
+        //2.画面遷移用ボタンにリスナを登録。
+        btnIntent.setOnClickListener (object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //3.Intentクラスのオブジェクトを生成。
+                val intent = Intent(this@Yonabaru, YonabaruStore::class.java)
+                //生成したオブジェクトを引数に画面を起動！
+                startActivity(intent)
+            }
+        })
     }
 }
