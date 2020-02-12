e7ca620742e4503e53353a7c5302989060612603
s18002 <s18002@std.it-college.ac.jp>
2020-02-12 16:08:25 +0900

    てすと5

--- app/src/main/java/com/example/soba/YonabaruStore.kt
+++ app/src/main/java/com/example/soba/YonabaruStore.kt
@@ -12,18 +12,6 @@ class YonabaruStore : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.soba.R.layout.activity_yonabaru_store)
-/*    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
- */
-        // MapFragmentの生成
-        val mapFragment = MapFragment.newInstance()
-        // MapViewをMapFragmentに変更する
-        val fragmentTransaction: FragmentTransaction =
-            fragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.mapView, mapFragment)
-        fragmentTransaction.commit()
-        mapFragment.getMapAsync(this)
     }
 
 }
